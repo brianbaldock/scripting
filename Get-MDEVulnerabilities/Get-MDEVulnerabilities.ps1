110e37e52eb109b65b770115a7ac8ead16746a9b
Brian Baldock <72090857+brianbaldock@users.noreply.github.com>
2022-05-18 10:26:23 -0400

    commit

--- Get-MDEVulnerabilities/Get-MDEVulnerabilities.ps1
+++ Get-MDEVulnerabilities/Get-MDEVulnerabilities.ps1
@@ -26,3 +26,59 @@
 
 #>
 
+
+
+$ScriptDir = Split-Path $script:MyInvocation.MyCommand.Path
+
+## Variables
+$systemmessagecolor = "cyan"
+$processmessagecolor = "green"
+
+# Application (client) ID, tenant ID and secret
+$clientid = "26179577-ab75-47ef-b076-0503a82b9e6e"
+$tenantid = "6e00d973-41f0-483c-a664-a98bd018a6fe"
+$clientsecret = "PY98Q~zAh-ad-BT9qrhn4t_Jh36Hs_VDaTIoqbw1"
+
+Clear-Host
+
+write-host -foregroundcolor $systemmessagecolor "Script started`n"
+
+# Construct URI
+$uri = "https://login.microsoftonline.com/$($tenantId)/oauth2/v2.0/token"
+
+# Construct Body
+$body = @{
+    client_id     = $clientId
+    scope         = "https://api.securitycenter.microsoft.com/.default"
+    client_secret = $clientSecret
+    grant_type    = "client_credentials"
+}
+
+write-host -foregroundcolor $processmessagecolor "Get OAuth 2.0 Token"
+# Get OAuth 2.0 Token
+$tokenRequest = Invoke-WebRequest -Method Post -Uri $uri -ContentType "application/x-www-form-urlencoded" -Body $body -UseBasicParsing
+
+# Access Token
+$token = ($tokenRequest.Content | ConvertFrom-Json).access_token
+
+# Graph API call in PowerShell using obtained OAuth token (see other gists for more details)
+
+# Specify the URI to call and method
+$uri1 = "https://api.securitycenter.microsoft.com/api/machines/SoftwareVulnerabilitiesByMachine"
+$uri2 = "https://api.securitycenter.microsoft.com/api/Vulnerabilities"
+$method = "GET"
+
+write-host -foregroundcolor $processmessagecolor "Run Graph API Query"
+# Run Graph API query 
+$query1 = Invoke-WebRequest -Method $method -Uri $uri1 -ContentType "application/json" -Headers @{Authorization = "Bearer $token" } -ErrorAction Stop -UseBasicParsing
+$query2 = Invoke-WebRequest -Method $method -Uri $uri2 -ContentType "application/json" -Headers @{Authorization = "Bearer $token" } -ErrorAction Stop -UseBasicParsing
+## Screen output
+write-host -foregroundcolor $processmessagecolor "Parse results"
+$ConvertedOutput1 = $query1.content | ConvertFrom-Json -AsHashtable
+$ConvertedOutput2 = $query2.content | ConvertFrom-Json -AsHashtable
+
+$MachineData = $ConvertedOutput1.value | select-object Devicename, cveid,lastseentimestamp,softwarename,softwarevendor, softwareversion,vulnerabilityseveritylevel
+$CVEData = $ConvertedOutput2.value | select-object id,name,severity,cvssv3,publishedon,updatedon
+
+
+write-host -foregroundcolor $systemmessagecolor "`nScript Completed`n"
